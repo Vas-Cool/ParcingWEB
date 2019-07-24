73c71d8053634b927ab468d2495eabf18324827d
Vas-Cool <vkulikov@inbox.ru>
2019-07-24 13:21:55 +0300

    HW 2 Requests Python Basic

---
+++ parse_wiki2.py
@@ -0,0 +1,22 @@
+mport collections
+import requests
+import re
+
+
+def return_wiki_html(topic):
+    wiki_request = requests.get(f'https://ru.wikipedia.org/wiki/{topic.capitalize()}')
+    return wiki_request.text
+
+def return_words(topic):
+    wiki_html = return_wiki_html(topic)
+    words = re.findall('<a href="[^>]+">.+?<\/a>', wiki_html)
+    words_counter = collections.Counter()
+    for word in words:
+        print(word)
+        words_counter[word] += 1
+    for word in words_counter.most_common(10):
+        print(f'Слово {word[0]} встречается {word[1]} раз')
+    return words_counter.most_common(10)
+
+
+print(return_words('Трям!_Здравствуйте!'))
